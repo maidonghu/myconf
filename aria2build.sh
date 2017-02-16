df9a7ea8355895373d2e1b367cd7972071c7e3fc
maidonghu <maidonghu@users.noreply.github.com>
2017-02-16 08:30:19 -0800

    Update aria2build.sh

--- aria2build.sh
+++ aria2build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo -e "\r" | add-apt-repository ppa:jonathonf/ffmpeg-3
-apt update && apt install ffmpeg aria2 -y
+echo -e "\r" | sudo add-apt-repository ppa:jonathonf/ffmpeg-3
+sudo apt update && sudo apt install ffmpeg aria2 -y
 wget https://raw.githubusercontent.com/maidonghu/myaria2/master/aria2_1.31.0-1_amd64.deb
-dpkg -i aria2_1.31.0-1_amd64.deb
+sudo dpkg -i aria2_1.31.0-1_amd64.deb
 cd /mnt/volume-nyc1-01
-mkdir -p Downloads
+sudo mkdir -p Downloads
 cd Downloads
-touch aria2.log
-touch aria2.session
+sudo touch aria2.log
+sudo touch aria2.session
 cd ~
 wget https://raw.githubusercontent.com/maidonghu/myaria2/master/aria2.conf
-mv aria2.conf /usr/local/etc/aria2.conf
-aria2c --conf-path=/usr/local/etc/aria2.conf
+sudo mv aria2.conf /usr/local/etc/aria2.conf
+sudo aria2c --conf-path=/usr/local/etc/aria2.conf
 
 #git clone https://github.com/aria2/aria2.git
 #apt install libgnutls-dev nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config
@@ -26,14 +26,14 @@ aria2c --conf-path=/usr/local/etc/aria2.conf
 #make distclean
 #cd ..
 
-apt install megatools -y
+sudo apt install megatools -y
 
-apt install golang-go -y
-echo export GOPATH=/root/gocode | tee -a ./.profile
-source ./.profile
-echo export PATH=$PATH:$GOPATH/bin | tee -a ./.profile
-source ./.profile
-go get github.com/prasmussen/gdrive
+sudo apt install golang-go -y
+echo export GOPATH=/root/gocode | sudo tee -a ./.profile
+sudo source ./.profile
+echo export PATH=$PATH:$GOPATH/bin | sudo tee -a ./.profile
+sudo source ./.profile
+sudo go get github.com/prasmussen/gdrive
 wget https://raw.githubusercontent.com/maidonghu/myaria2/master/token_v2.json
-mkdir .gdrive
-mv token_v2.json .gdrive/token_v2.json
+sudo mkdir .gdrive
+sudo mv token_v2.json ./.gdrive/token_v2.json
